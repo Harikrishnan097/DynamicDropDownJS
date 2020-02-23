b6883a56e1129ebe08d9e823f5175162927ff753
Harikrishnan <31622792+Harikrishnan097@users.noreply.github.com>
2020-02-23 12:42:02 +0530

    Add files via upload

---
+++ REgistratoinform/src/main/java/com/spring/websiteacty/controller/RegController.java
@@ -0,0 +1,20 @@
+package com.spring.websiteacty.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class RegController {
+
+	
+	
+	@RequestMapping("/")
+	public ModelAndView page()
+	{
+		
+		return new ModelAndView("index");
+	}
+	
+}
